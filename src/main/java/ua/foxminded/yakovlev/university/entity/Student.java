0243d743604417dcda8ce535c499778af2c3b0e2
Alexander Yakovlev <sashayassa@gmail.com>
2020-10-12 11:16:00 +0300

    initial Student

---
+++ src/main/java/ua/foxminded/yakovlev/university/entity/Student.java
@@ -0,0 +1,63 @@
+package ua.foxminded.yakovlev.university.entity;
+
+public class Student extends Person {
+
+	private Group group;
+
+	public Student() {
+		super();
+	}
+
+	public Group getGroup() {
+		return group;
+	}
+
+	public void setGroup(Group group) {
+		this.group = group;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((group == null) ? 0 : group.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Student other = (Student) obj;
+		if (group == null) {
+			if (other.group != null) {
+				return false;
+			}
+		} else if (!group.equals(other.group)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Student [group=");
+		builder.append(group);
+		builder.append(", getId()=");
+		builder.append(getId());
+		builder.append(", getFirstName()=");
+		builder.append(getFirstName());
+		builder.append(", getLastName()=");
+		builder.append(getLastName());
+		builder.append("]");
+		return builder.toString();
+	}
+}
