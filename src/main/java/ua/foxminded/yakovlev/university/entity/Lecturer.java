729e2d09c6e3baaf65850be1068f957172134821
Alexander Yakovlev <sashayassa@gmail.com>
2020-10-12 11:43:54 +0300

    updating Lecturer: adding id

--- src/main/java/ua/foxminded/yakovlev/university/entity/Lecturer.java
+++ src/main/java/ua/foxminded/yakovlev/university/entity/Lecturer.java
@@ -2,11 +2,20 @@ package ua.foxminded.yakovlev.university.entity;
 
 public class Lecturer extends Person {
 
+	private Long id;
 	private Position position;
 	
     public Lecturer() {
     	super();
     }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public Position getPosition() {
 		return position;
@@ -20,6 +29,7 @@ public class Lecturer extends Person {
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((position == null) ? 0 : position.hashCode());
 		return result;
 	}
@@ -36,6 +46,13 @@ public class Lecturer extends Person {
 			return false;
 		}
 		Lecturer other = (Lecturer) obj;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
 		if (position != other.position) {
 			return false;
 		}
@@ -45,14 +62,10 @@ public class Lecturer extends Person {
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("Lecturer [position=");
+		builder.append("Lecturer [id=");
+		builder.append(id);
+		builder.append(", position=");
 		builder.append(position);
-		builder.append(", getId()=");
-		builder.append(getId());
-		builder.append(", getFirstName()=");
-		builder.append(getFirstName());
-		builder.append(", getLastName()=");
-		builder.append(getLastName());
 		builder.append("]");
 		return builder.toString();
 	}
