5f34b2ca8b474691747a475fec5cab01a50173e5
Alexander Yakovlev <sashayassa@gmail.com>
2020-10-13 14:35:28 +0300

    refactoring all the entities add lombok annotations

--- src/main/java/ua/foxminded/yakovlev/university/entity/Lecturer.java
+++ src/main/java/ua/foxminded/yakovlev/university/entity/Lecturer.java
@@ -1,72 +1,13 @@
 package ua.foxminded.yakovlev.university.entity;
 
-public class Lecturer extends Person {
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Builder
+@EqualsAndHashCode(callSuper=true)
+@Data public class Lecturer extends Person {
 
 	private Long id;
 	private Position position;
-	
-    public Lecturer() {
-    	super();
-    }
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Position getPosition() {
-		return position;
-	}
-
-	public void setPosition(Position position) {
-		this.position = position;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((position == null) ? 0 : position.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!super.equals(obj)) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Lecturer other = (Lecturer) obj;
-		if (id == null) {
-			if (other.id != null) {
-				return false;
-			}
-		} else if (!id.equals(other.id)) {
-			return false;
-		}
-		if (position != other.position) {
-			return false;
-		}
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Lecturer [id=");
-		builder.append(id);
-		builder.append(", position=");
-		builder.append(position);
-		builder.append("]");
-		return builder.toString();
-	}
 }
--- src/main/java/ua/foxminded/yakovlev/university/entity/Student.java
+++ src/main/java/ua/foxminded/yakovlev/university/entity/Student.java
@@ -1,76 +1,13 @@
 package ua.foxminded.yakovlev.university.entity;
 
-public class Student extends Person {
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Builder
+@EqualsAndHashCode(callSuper=true)
+@Data public class Student extends Person {
 
 	private Long id;
 	private Group group;
-
-	public Student() {
-		super();
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Group getGroup() {
-		return group;
-	}
-
-	public void setGroup(Group group) {
-		this.group = group;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + ((group == null) ? 0 : group.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!super.equals(obj)) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Student other = (Student) obj;
-		if (group == null) {
-			if (other.group != null) {
-				return false;
-			}
-		} else if (!group.equals(other.group)) {
-			return false;
-		}
-		if (id == null) {
-			if (other.id != null) {
-				return false;
-			}
-		} else if (!id.equals(other.id)) {
-			return false;
-		}
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Student [id=");
-		builder.append(id);
-		builder.append(", group=");
-		builder.append(group);
-		builder.append("]");
-		return builder.toString();
-	}
 }
