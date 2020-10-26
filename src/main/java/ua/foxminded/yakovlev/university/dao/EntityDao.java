294fc48d00d1bb90bccce638905ac2096b1869c7
Alexander Yakovlev <sashayassa@gmail.com>
2020-10-26 19:02:04 +0300

    Update EntityDao

--- src/main/java/ua/foxminded/yakovlev/university/dao/EntityDao.java
+++ src/main/java/ua/foxminded/yakovlev/university/dao/EntityDao.java
@@ -2,13 +2,11 @@ package ua.foxminded.yakovlev.university.dao;
 
 import java.util.List;
 
-import ua.foxminded.yakovlev.university.exception.DaoNotFoundException;
-
 public interface EntityDao<E, ID> {
 
-	void save(E entity);
+	E save(E entity);
 	E findById(ID id);
 	List<E> findAll();
-	void update(E entity);
+	E update(E entity);
 	void delete(ID id);
 }
