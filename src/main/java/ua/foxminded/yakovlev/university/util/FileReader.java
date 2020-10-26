c284c8f40d9a5cbbc417eb6e45dc0a3d16372c77
Alexander Yakovlev <sashayassa@gmail.com>
2020-10-26 19:03:34 +0300

    initial FileReader

---
+++ src/main/java/ua/foxminded/yakovlev/university/util/FileReader.java
@@ -0,0 +1,37 @@
+package ua.foxminded.yakovlev.university.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import ua.foxminded.yakovlev.university.exception.DaoRuntimeException;
+
+public class FileReader {
+
+	public List<String> readFile(Path path) {
+		
+		checkFile(path);
+		
+		try {
+			return Files.readAllLines(path);
+		} catch (IOException e) {			
+			throw new DaoRuntimeException("can't read " + path.toString(), e);
+		}		
+	}
+	
+	private void checkFile(Path path) {
+		
+		if (path == null) {
+			throw new DaoRuntimeException("file is null");
+		}
+		
+		if (!Files.exists(path)) {
+			throw new DaoRuntimeException(path.toString() + " does not exist");
+		}
+		
+		if (!Files.isReadable(path)) {
+			throw new DaoRuntimeException(path.toString() + " is not readable");
+		}
+	}
+}
