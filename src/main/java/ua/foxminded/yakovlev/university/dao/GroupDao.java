9d7919368fc915145be8a176dd52bad138a76ba7
Alexander Yakovlev <sashayassa@gmail.com>
2020-10-30 17:18:12 +0300

    update GroupDao and GroupDaoImpl

--- src/main/java/ua/foxminded/yakovlev/university/dao/GroupDao.java
+++ src/main/java/ua/foxminded/yakovlev/university/dao/GroupDao.java
@@ -1,8 +1,14 @@
 package ua.foxminded.yakovlev.university.dao;
 
+import java.util.List;
+
 import ua.foxminded.yakovlev.university.entity.Group;
 
 public interface GroupDao extends EntityDao<Group, Long> {
 
 	Group findGroupByName(String groupName);
+	
+	List<Group> findByTimeableId(Long id);
+	List<Group> addToTimeable(Long timetableId, Long groupId);
+	List<Group> removeFromTimeable(Long timetableId, Long groupId);
 }
--- src/main/java/ua/foxminded/yakovlev/university/dao/impl/GroupDaoImpl.java
+++ src/main/java/ua/foxminded/yakovlev/university/dao/impl/GroupDaoImpl.java
@@ -1,6 +1,7 @@
 package ua.foxminded.yakovlev.university.dao.impl;
 
 import java.sql.PreparedStatement;
+import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
@@ -22,6 +23,15 @@ public class GroupDaoImpl extends AbstractDao<Group, Long> implements GroupDao {
 			+ "WHERE group_id=?;";
 	private static final String DELETE = "DELETE FROM public.groups WHERE group_id = ?;";
 	private static final String FIND_BY_GROUP_NAME = "SELECT * FROM public.groups WHERE group_name = ?;";
+	private static final String FIND_BY_TIMETABLE_ID = "SELECT g.* "
+			+ "FROM public.timetable_record_groups t "
+			+ "JOIN public.groups g ON t.timetable_record_group_group_id = g.group_id "
+			+ "WHERE t.timetable_record_group_timetable_record_id = ?;";
+	private static final String ADD_TO_TIMETABLE = "INSERT INTO public.timetable_record_groups( "
+			+ "timetable_record_group_timetable_record_id, timetable_record_group_group_id) "
+			+ "VALUES (?, ?);";
+	private static final String REMOVE_FROM_TIMETABLE = "DELETE FROM public.timetable_record_groups t "
+			+ "WHERE t.timetable_record_group_timetable_record_id = ? AND t.timetable_record_group_group_id = ?;";
 	private static final String ID_KEY = "group_id";
 	
 	private final JdbcTemplate jdbcTemplate;
@@ -59,5 +69,35 @@ public class GroupDaoImpl extends AbstractDao<Group, Long> implements GroupDao {
 	                ps.setLong(2, group.getId());
 	                return ps;
 	              };
+	}
+
+	@Override
+	public List<Group> findByTimeableId(Long id) {
+				
+		return findByQuery(FIND_BY_TIMETABLE_ID, ps -> {
+			ps.setLong(1, id);
+		});
+	}
+
+	@Override
+	public List<Group> addToTimeable(Long timetableId, Long groupId) {
+		
+		executeUpdate(ADD_TO_TIMETABLE, ps -> {
+			ps.setLong(1, timetableId);
+			ps.setLong(2, groupId);
+		});
+		
+		return findByTimeableId(timetableId);
+	}
+
+	@Override
+	public List<Group> removeFromTimeable(Long timetableId, Long groupId) {
+		
+		executeUpdate(REMOVE_FROM_TIMETABLE, ps -> {
+			ps.setLong(1, timetableId);
+			ps.setLong(2, groupId);
+		});
+		
+		return findByTimeableId(timetableId);
 	}
 }
