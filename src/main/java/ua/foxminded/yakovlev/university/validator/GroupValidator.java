b2b0b0fd970bd66ce82ba1a38cca0b7ca603808a
Alexander Yakovlev <sashayassa@gmail.com>
2020-11-02 15:54:01 +0300

    update EntityValidator and GroupValidator

--- src/main/java/ua/foxminded/yakovlev/university/validator/GroupValidator.java
+++ src/main/java/ua/foxminded/yakovlev/university/validator/GroupValidator.java
@@ -5,12 +5,12 @@ import org.springframework.stereotype.Component;
 import ua.foxminded.yakovlev.university.entity.Group;
 
 @Component
-public class GroupValidator extends EntityValidator<Group>{
+public class GroupValidator extends EntityValidator<Group, Long>{
 	
 	private static final String NAME_PATTERN = "^[a-z]{2}[-][0-9]{2}$";
 
 	@Override
-	public void checkSpetialEntityRequirements(Group group) {
+	public void checkSpecialEntityRequirements(Group group) {
 
 		checkForNullOrEmptyProperty(group.getName());
 		checkByPattern(group.getName(), NAME_PATTERN);
