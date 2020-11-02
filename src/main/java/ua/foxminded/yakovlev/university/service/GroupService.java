ab65e76e578584250494832753e653a18d6b584a
Alexander Yakovlev <sashayassa@gmail.com>
2020-11-02 15:52:32 +0300

    updating GroupService and GroupServiceImpl

--- src/main/java/ua/foxminded/yakovlev/university/service/GroupService.java
+++ src/main/java/ua/foxminded/yakovlev/university/service/GroupService.java
@@ -1,16 +1,8 @@
 package ua.foxminded.yakovlev.university.service;
 
-import java.util.List;
-
 import ua.foxminded.yakovlev.university.entity.Group;
 
-public interface GroupService {
-		
-	Group save(Group entity);
-	Group findById(Long id);
-	List<Group> findAll();
-	Group update(Group entity);
-	void delete(Long id);
+public interface GroupService extends EntityService<Group, Long> {
 	
 	Group findGroupByName(String groupName);
 }
