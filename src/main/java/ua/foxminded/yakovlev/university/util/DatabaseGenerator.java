76c95e0b2ad1613a93a5f352d320072e6887bac4
Alexander Yakovlev <sashayassa@gmail.com>
2020-11-05 15:47:43 +0300

    replacing DatabaseGenerator

--- src/main/java/ua/foxminded/yakovlev/university/util/DatabaseGenerator.java
+++ src/main/java/ua/foxminded/yakovlev/university/util/DatabaseGenerator.java
@@ -1,26 +1,25 @@
-package ua.foxminded.yakovlev.university.testutil;
+package ua.foxminded.yakovlev.university.util;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import ua.foxminded.yakovlev.university.dao.impl.ScriptExecutor;
-import ua.foxminded.yakovlev.university.util.FileReader;
 
-public class TestDatabaseGenerator {
-	
-	private static final String TEST_DATABASE_SCRIPT_PATH = "src/test/resources/university_test_db_initialiser.sql";
+public class DatabaseGenerator {
 	
 	private final FileReader fileReader;
 	private final ScriptExecutor scriptExecutor;
+	private final String scriptFile;
 	
-	public TestDatabaseGenerator(FileReader fileReader, ScriptExecutor scriptExecutor) {
+	public DatabaseGenerator(FileReader fileReader, ScriptExecutor scriptExecutor, String scriptPath) {
 		this.fileReader = fileReader;
 		this.scriptExecutor = scriptExecutor;
-	}
+		this.scriptFile =  scriptPath;
+		}
 
 	public void generate() {
 		
-		Path scriptPath = Paths.get(TEST_DATABASE_SCRIPT_PATH);
+		Path scriptPath = Paths.get(scriptFile);
 		String script = String.join("\n", fileReader.readFile(scriptPath));
 		scriptExecutor.execute(script);
 	}
