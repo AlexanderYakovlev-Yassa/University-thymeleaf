b9646404da19df924ac98f8b07df053d4df30a94
Alexander Yakovlev <sashayassa@gmail.com>
2020-11-30 19:42:21 +0300

    updating AppConfiguration

--- src/main/java/ua/foxminded/yakovlev/university/init/AppConfiguration.java
+++ src/main/java/ua/foxminded/yakovlev/university/init/AppConfiguration.java
@@ -1,32 +1,31 @@
 package ua.foxminded.yakovlev.university.init;
 
+import java.util.Properties;
+
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jndi.JndiTemplate;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import ua.foxminded.yakovlev.university.dao.CourseDao;
-import ua.foxminded.yakovlev.university.dao.GroupDao;
-import ua.foxminded.yakovlev.university.dao.LecturerDao;
-import ua.foxminded.yakovlev.university.dao.PositionDao;
-import ua.foxminded.yakovlev.university.dao.StudentDao;
 import ua.foxminded.yakovlev.university.dao.TimetableRecordDao;
-import ua.foxminded.yakovlev.university.dao.impl.CourseDaoImpl;
-import ua.foxminded.yakovlev.university.dao.impl.GroupDaoImpl;
-import ua.foxminded.yakovlev.university.dao.impl.LecturerDaoImpl;
-import ua.foxminded.yakovlev.university.dao.impl.PositionDaoImpl;
 import ua.foxminded.yakovlev.university.dao.impl.ScriptExecutor;
-import ua.foxminded.yakovlev.university.dao.impl.StudentDaoImpl;
 import ua.foxminded.yakovlev.university.dao.impl.TimetableRecordDaoImpl;
-import ua.foxminded.yakovlev.university.mapper.CourseMapper;
+import ua.foxminded.yakovlev.university.jpaDao.CourseRepository;
+import ua.foxminded.yakovlev.university.jpaDao.GroupRepository;
+import ua.foxminded.yakovlev.university.jpaDao.LecturerRepository;
+import ua.foxminded.yakovlev.university.jpaDao.PositionRepository;
+import ua.foxminded.yakovlev.university.jpaDao.StudentRepository;
 import ua.foxminded.yakovlev.university.mapper.GroupMapper;
-import ua.foxminded.yakovlev.university.mapper.LecturerMapper;
-import ua.foxminded.yakovlev.university.mapper.PositionMapper;
-import ua.foxminded.yakovlev.university.mapper.StudentMapper;
 import ua.foxminded.yakovlev.university.mapper.TimetableRecordMapper;
 import ua.foxminded.yakovlev.university.service.CourseService;
 import ua.foxminded.yakovlev.university.service.GroupService;
@@ -43,81 +42,90 @@ import ua.foxminded.yakovlev.university.service.impl.TimetableRecordServiceImpl;
 
 @Configuration
 @ComponentScan("ua.foxminded.yakovlev.university")
+@EnableTransactionManagement
 public class AppConfiguration {
 
-	@Bean (name="jdbcTemplate")
-	public JdbcTemplate getJdbcTemplate() throws NamingException {
-		
+	@Bean
+	public DataSource getDataSource() throws NamingException {
+
 		JndiTemplate jndiTemplate = new JndiTemplate();
 		DataSource dataSource = (DataSource) jndiTemplate.lookup("java:comp/env/jdbc/UniversityDB");
-		
-		return new JdbcTemplate(dataSource);
+
+		return dataSource;
 	}
-	
-	@Bean (name="scriptExecutor")
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws NamingException {
+		LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+		entityManager.setDataSource(getDataSource());
+		entityManager.setPackagesToScan(new String[] { "ua.foxminded.yakovlev.university.entity" });
+
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		entityManager.setJpaVendorAdapter(vendorAdapter);
+		entityManager.setJpaProperties(additionalProperties());
+
+		return entityManager;
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager() throws NamingException {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+		return transactionManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	private Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+
+		return properties;
+	}
+
+	@Bean(name = "scriptExecutor")
 	public ScriptExecutor getScriptExecutor(JdbcTemplate jdbcTemplate) {
 		return new ScriptExecutor(jdbcTemplate);
 	}
-	
-	@Bean (name="courseDao")
-	public CourseDao getCourseDao(JdbcTemplate jdbcTemplate, CourseMapper courseMapper) {		
-		return new CourseDaoImpl(jdbcTemplate, courseMapper);
-	}
-	
-	@Bean (name="groupDao")
-	public GroupDao getGroupDao(JdbcTemplate jdbcTemplate, GroupMapper groupMapper) {		
-		return new GroupDaoImpl(jdbcTemplate, groupMapper);
-	}
-	
-	@Bean (name="positionDao")
-	public PositionDao getPositionDao(JdbcTemplate jdbcTemplate, PositionMapper positionMapper) {		
-		return new PositionDaoImpl(jdbcTemplate, positionMapper);
-	}
-	
-	@Bean (name="lecturerDao")
-	public LecturerDao getLecturerDao(JdbcTemplate jdbcTemplate, LecturerMapper lecturerMapper) {		
-		return new LecturerDaoImpl(jdbcTemplate, lecturerMapper);
-	}
-	
-	@Bean (name="studentDao")
-	public StudentDao getStudentDao(JdbcTemplate jdbcTemplate, StudentMapper studentMapper) {		
-		return new StudentDaoImpl(jdbcTemplate, studentMapper);
-	}
-	
-	@Bean (name="timetableRecordDao")
-	public TimetableRecordDao getTimetableRecordDao(JdbcTemplate jdbcTemplate, 
-			TimetableRecordMapper timetableRecordMapper,
-			GroupMapper groupMapper) {		
+
+	@Bean(name = "timetableRecordDao")
+	public TimetableRecordDao getTimetableRecordDao(JdbcTemplate jdbcTemplate,
+			TimetableRecordMapper timetableRecordMapper, GroupMapper groupMapper) {
 		return new TimetableRecordDaoImpl(jdbcTemplate, timetableRecordMapper, groupMapper);
 	}
-	
-	@Bean (name="courseService")
-	public CourseService getCourseService(CourseDao courseDao) {		
-		return new CourseServiceImpl(courseDao);
+
+	@Bean(name = "courseService")
+	public CourseService getCourseService(CourseRepository courseRepository) {
+		return new CourseServiceImpl(courseRepository);
 	}
-	
-	@Bean (name="groupService")
-	public GroupService getGroupService(GroupDao groupDao) {		
-		return new GroupServiceImpl(groupDao);
+
+	@Bean(name = "groupService")
+	public GroupService getGroupService(GroupRepository groupRepository) {
+		return new GroupServiceImpl(groupRepository);
 	}
-	
-	@Bean (name="positionService")
-	public PositionService getPositionService(PositionDao positionDao) {		
-		return new PositionServiceImpl(positionDao);
+
+	@Bean(name = "positionService")
+	public PositionService getPositionService(PositionRepository positionRepository) {
+		return new PositionServiceImpl(positionRepository);
 	}
-	
-	@Bean (name="studentService")
-	public StudentService getStudentService(StudentDao studentDao) {		
-		return new StudentServiceImpl(studentDao);
+
+	@Bean(name = "studentService")
+	public StudentService getStudentService(StudentRepository studentRepository) {
+		return new StudentServiceImpl(studentRepository);
 	}
-	
-	@Bean (name="lecturerService")
-	public LecturerService getLecturerService(LecturerDao lecturerDao) {		
-		return new LecturerServiceImpl(lecturerDao);
+
+	@Bean(name = "lecturerService")
+	public LecturerService getLecturerService(LecturerRepository lecturerRepository) {
+		return new LecturerServiceImpl(lecturerRepository);
 	}
-	
-	@Bean (name="timetableRecordService")
-	public TimetableRecordService getTimetableRecordService(TimetableRecordDao timetableRecordDao) {		
+
+	@Bean(name = "timetableRecordService")
+	public TimetableRecordService getTimetableRecordService(TimetableRecordDao timetableRecordDao) {
 		return new TimetableRecordServiceImpl(timetableRecordDao);
 	}
 }
