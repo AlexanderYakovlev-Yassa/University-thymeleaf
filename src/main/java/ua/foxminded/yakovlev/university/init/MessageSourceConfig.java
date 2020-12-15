26cc0668c405fb1788a81be9aaf79cd75e2e6b93
Alexander Yakovlev <sashayassa@gmail.com>
2020-12-15 22:09:09 +0300

    initial MessageSourceConfig

---
+++ src/main/java/ua/foxminded/yakovlev/university/init/MessageSourceConfig.java
@@ -0,0 +1,18 @@
+package ua.foxminded.yakovlev.university.init;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class MessageSourceConfig {
+
+	@Bean("messageSource")
+	public ResourceBundleMessageSource getMessageSource() {
+		
+		String message = "message";
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename(message);
+		return messageSource;		
+	}
+}
