6a34442a76a092bf29e3fef4019746b06dbecfed
Alexander Yakovlev <sashayassa@gmail.com>
2020-12-15 22:07:54 +0300

    initial validators: PersonValidator StudentValidator

---
+++ src/main/java/ua/foxminded/yakovlev/university/validator/PersonValidator.java
@@ -0,0 +1,24 @@
+package ua.foxminded.yakovlev.university.validator;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import ua.foxminded.yakovlev.university.entity.Person;
+
+@Component
+public class PersonValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Person.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object obj, Errors errors) {
+		//Person person = (Person) obj;
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "validator.message.empty_first_name");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "validator.message.empty_last_name");
+	}
+}
