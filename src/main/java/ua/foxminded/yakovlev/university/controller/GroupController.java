36bb41ee0434beab760e7a5f724fe05c0534973a
Alexander Yakovlev <sashayassa@gmail.com>
2020-12-16 18:59:08 +0300

    add group validation
    application.properties
    GroupController.java
    message.properties
    message_ru_RU.properties
    show-groups.html

--- src/main/java/ua/foxminded/yakovlev/university/controller/GroupController.java
+++ src/main/java/ua/foxminded/yakovlev/university/controller/GroupController.java
@@ -1,7 +1,14 @@
 package ua.foxminded.yakovlev.university.controller;
 
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.DataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 import ua.foxminded.yakovlev.university.entity.Group;
 import ua.foxminded.yakovlev.university.service.GroupService;
 import ua.foxminded.yakovlev.university.service.StudentService;
+import ua.foxminded.yakovlev.university.validator.GroupValidator;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,21 +28,52 @@ public class GroupController {
 	
 	private final GroupService groupService;
 	private final StudentService studentService;
+	private final GroupValidator groupValidator;
+	@Qualifier(value="messageSource")
+	private final ResourceBundleMessageSource messageSource;
 	
 	@GetMapping()
-    public String showPositions(Model model) {
+    public String showPositions(
+			@RequestParam(name = "errorMessage", required = false) List<String> errorMessageList,
+			@RequestParam(name = "activeWindow", required = false) String activeWindow,
+			@RequestParam(name = "editingGroupId", required = false) Long editingGroupId,
+			@RequestParam(name = "editingGroupName", required = false) String editingGroupName,
+			Model model) {
 		
 		model.addAttribute("groups", groupService.findAll());
+		model.addAttribute("errorMessageList", errorMessageList);
+		model.addAttribute("activeWindow", activeWindow);
+		model.addAttribute("editingGroupId", editingGroupId);
+		model.addAttribute("editingGroupName", editingGroupName);
 		
         return "groups/show-groups";
     }
 	
 	@PostMapping("/new")
     public String save(
+			RedirectAttributes redirectAttributes,			
     		@RequestParam(name = "name") String name) {
 		
 		Group group = new Group();
 		group.setName(name);
+		
+		final DataBinder dataBinder = new DataBinder(group);
+		dataBinder.addValidators(groupValidator);
+		dataBinder.validate();
+		
+		if (dataBinder.getBindingResult().hasErrors()) {
+			
+			List<String> errorMessageList = dataBinder.getBindingResult().getAllErrors().stream().
+					map(e -> (messageSource.getMessage(e, Locale.getDefault())))
+					.collect(Collectors.toList());
+			
+			redirectAttributes.addAttribute("errorMessage", errorMessageList);
+			redirectAttributes.addAttribute("activeWindow", "save");
+			redirectAttributes.addAttribute("editingGroupName", name);
+			
+			return "redirect:/groups";
+		}
+		
 		groupService.save(group);
 		
 		return "redirect:/groups";
@@ -48,12 +87,32 @@ public class GroupController {
 	
 	@PostMapping("/edit")
     public String edit(
-    		@RequestParam(name = "id") Long id,
+			RedirectAttributes redirectAttributes,
+			@RequestParam(name = "id") Long id,
     		@RequestParam(name = "name") String name) {
 		
 		Group group = new Group();
 		group.setId(id);
 		group.setName(name);
+		
+		final DataBinder dataBinder = new DataBinder(group);
+		dataBinder.addValidators(groupValidator);
+		dataBinder.validate();
+		
+		if (dataBinder.getBindingResult().hasErrors()) {
+			
+			List<String> errorMessageList = dataBinder.getBindingResult().getAllErrors().stream().
+					map(e -> (messageSource.getMessage(e, Locale.getDefault())))
+					.collect(Collectors.toList());
+			
+			redirectAttributes.addAttribute("errorMessage", errorMessageList);
+			redirectAttributes.addAttribute("activeWindow", "edit");
+			redirectAttributes.addAttribute("editingGroupId", id);
+			redirectAttributes.addAttribute("editingGroupName", name);
+			
+			return "redirect:/groups";
+		}
+		
 		groupService.update(group);
 		
 		return "redirect:/groups";
