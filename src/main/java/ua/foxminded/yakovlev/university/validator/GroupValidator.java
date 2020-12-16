6a9bf11c7005d979f9c36d93634521ab2e0189d0
Alexander Yakovlev <sashayassa@gmail.com>
2020-12-16 18:57:51 +0300

    initial GroupValidator

---
+++ src/main/java/ua/foxminded/yakovlev/university/validator/GroupValidator.java
@@ -0,0 +1,38 @@
+package ua.foxminded.yakovlev.university.validator;
+
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import ua.foxminded.yakovlev.university.entity.Group;
+
+@Component
+public class GroupValidator implements Validator {
+	
+	@Value(value = "${group.name.pattern}")
+	private String namePattern;
+
+	@Override
+	public boolean supports(Class<?> clazz) {		
+		return Group.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		
+		Group group = (Group) target;
+		Pattern pattern = Pattern.compile(namePattern);
+		
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "validator.message.empty_group_name");
+		
+		if (!pattern.matcher(group.getName()).matches()) {
+	        errors.rejectValue("name", "validator.message.name_pattern_not_matches");
+	    }
+	}
+
+	
+}
