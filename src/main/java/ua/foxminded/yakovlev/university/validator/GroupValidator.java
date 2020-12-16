5fc0c7cb56fe9a28f43f9ccba8171782d24c2edd
Alexander Yakovlev <sashayassa@gmail.com>
2020-12-16 20:11:48 +0300

    updating GroupValidator.java

--- src/main/java/ua/foxminded/yakovlev/university/validator/GroupValidator.java
+++ src/main/java/ua/foxminded/yakovlev/university/validator/GroupValidator.java
@@ -12,27 +12,28 @@ import ua.foxminded.yakovlev.university.entity.Group;
 
 @Component
 public class GroupValidator implements Validator {
-	
+
 	@Value(value = "${group.name.pattern}")
 	private String namePattern;
 
 	@Override
-	public boolean supports(Class<?> clazz) {		
+	public boolean supports(Class<?> clazz) {
 		return Group.class.equals(clazz);
 	}
 
 	@Override
 	public void validate(Object target, Errors errors) {
-		
+
 		Group group = (Group) target;
 		Pattern pattern = Pattern.compile(namePattern);
-		
+
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "validator.message.empty_group_name");
-		
-		if (!pattern.matcher(group.getName()).matches()) {
-	        errors.rejectValue("name", "validator.message.name_pattern_not_matches");
-	    }
+
+		if (errors.getFieldErrorCount("name") == 0) {
+			if (!pattern.matcher(group.getName()).matches()) {
+				errors.rejectValue("name", "validator.message.name_pattern_not_matches");
+			}
+		}
 	}
 
-	
 }
