861344e3244b7b84ca83bd50344aad66c70ead9f
Alexander Yakovlev <sashayassa@gmail.com>
2020-12-18 14:48:46 +0300

    add validation constrains: Course

--- src/main/java/ua/foxminded/yakovlev/university/entity/Course.java
+++ src/main/java/ua/foxminded/yakovlev/university/entity/Course.java
@@ -8,6 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -24,8 +26,11 @@ public class Course implements Serializable {
 	private Long id;
 	
 	@Column(name = "course_name")
+	@NotBlank(message="validator.message.empty_course_name")
+	//@NotBlank(message="vvv")
 	private String name;
 	
 	@Column(name = "course_description")
+	@Size(max = 1024, message = "validator.message.course_description_too_long")
 	private String description;
 }
