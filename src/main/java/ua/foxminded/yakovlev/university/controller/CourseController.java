f8493d51092cd5251837a5de8ca37c9f2901d49d
Alexander Yakovlev <sashayassa@gmail.com>
2020-12-18 14:50:22 +0300

    updating controller/CourseController

--- src/main/java/ua/foxminded/yakovlev/university/controller/CourseController.java
+++ src/main/java/ua/foxminded/yakovlev/university/controller/CourseController.java
@@ -2,14 +2,21 @@ package ua.foxminded.yakovlev.university.controller;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Valid;
+
+import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.DataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +26,6 @@ import lombok.RequiredArgsConstructor;
 import ua.foxminded.yakovlev.university.entity.Course;
 import ua.foxminded.yakovlev.university.service.CourseService;
 import ua.foxminded.yakovlev.university.validator.CourseValidator;
-import ua.foxminded.yakovlev.university.validator.GroupValidator;
 
 @Controller
 @RequiredArgsConstructor
@@ -34,54 +40,49 @@ public class CourseController {
 	@GetMapping()
     public String show(
 			@RequestParam(name = "errorMessage", required = false) List<String> errorMessageList,
-			@RequestParam(name = "activeWindow", required = false) String activeWindow,
-			@RequestParam(name = "editingCourseId", required = false) Long editingCourseId,
-			@RequestParam(name = "editingCourseName", required = false) String editingCourseName,
-			@RequestParam(name = "editingCourseDescription", required = false) String editingCourseDescription,
+			@RequestParam(name = "editingCourseId", required = false) Course editingCourse,
 			Model model) {
 
 		model.addAttribute("courses", courseService.findAll());
 		model.addAttribute("errorMessageList", errorMessageList);
-		model.addAttribute("activeWindow", activeWindow);
-		model.addAttribute("editingCourseId", editingCourseId);
-		model.addAttribute("editingCourseName", editingCourseName);
-		model.addAttribute("editingCourseDescription", editingCourseDescription);
+		model.addAttribute("editingCourse", editingCourse);
 		
         return "courses/show-courses";
     }
 	
+	@GetMapping("/new")
+	public String create(
+			@RequestParam(name = "errorMessage", required = false) List<String> errorMessageList,
+			Model model) {
+		
+		model.addAttribute("course", new Course());
+		model.addAttribute("errorMessageList", errorMessageList);
+		
+		return "courses/new-course";
+	}
+	
 	@PostMapping("/new")
     public String save(
 			RedirectAttributes redirectAttributes,
-    		@RequestParam(name = "name") String name,
-    		@RequestParam(name = "description") String description) {
+    		@Valid@ModelAttribute("course") Course  course,
+    		BindingResult bindingResult,
+			Model model) {		
 		
-		Course course = new Course();
-		course.setName(name);
-		course.setDescription(description);
-		
-		final DataBinder dataBinder = new DataBinder(course);
-		dataBinder.addValidators(courseValidator);
-		dataBinder.validate();
-		
-		if (dataBinder.getBindingResult().hasErrors()) {
+		if (bindingResult.hasErrors()) {
 			
-			List<String> errorMessageList = dataBinder.getBindingResult().getAllErrors().stream().
-					map(e -> (messageSource.getMessage(e, Locale.getDefault())))
+			List<String> errorMessageList = bindingResult.getAllErrors().stream().
+					map(e -> (messageSource.getMessage(e.getDefaultMessage(), null, Locale.getDefault())))
 					.collect(Collectors.toList());
 			
 			redirectAttributes.addAttribute("errorMessage", errorMessageList);
-			redirectAttributes.addAttribute("activeWindow", "save");
-			redirectAttributes.addAttribute("editingCourseName", name);
-			redirectAttributes.addAttribute("editingCourseDescription", description);
 			
-			return "redirect:/courses";
+			return "redirect:/courses/new";
 		}
 		
 		courseService.save(course);
 		
 		return "redirect:/courses";
-	}
+	}	
 	
 	@PostMapping("/delete")
     public String delete(@RequestParam(name = "id") long id) {
@@ -90,38 +91,38 @@ public class CourseController {
     }
 	
 	@PostMapping("/edit")
-    public String edit(
+	public String showEdit(
 			RedirectAttributes redirectAttributes,
-    		@RequestParam(name = "id") Long id,
-    		@RequestParam(name = "name") String name,
-    		@RequestParam(name = "description") String description) {
+			@Valid@ModelAttribute("course") Course course,
+    		BindingResult bindingResult) {
 		
-		Course course = new Course();
-		course.setId(id);
-		course.setName(name);
-		course.setDescription(description);
-		
-		final DataBinder dataBinder = new DataBinder(course);
-		dataBinder.addValidators(courseValidator);
-		dataBinder.validate();
-		
-		if (dataBinder.getBindingResult().hasErrors()) {
+		if (bindingResult.hasErrors()) {
 			
-			List<String> errorMessageList = dataBinder.getBindingResult().getAllErrors().stream().
-					map(e -> (messageSource.getMessage(e, Locale.getDefault())))
+			List<String> errorMessageList = bindingResult.getAllErrors().stream().
+					map(e -> (messageSource.getMessage(e.getDefaultMessage(), null, Locale.getDefault())))
 					.collect(Collectors.toList());
 			
 			redirectAttributes.addAttribute("errorMessage", errorMessageList);
-			redirectAttributes.addAttribute("activeWindow", "edit");
-			redirectAttributes.addAttribute("editingCourseId", id);
-			redirectAttributes.addAttribute("editingCourseName", name);
-			redirectAttributes.addAttribute("editingCourseDescription", description);
+			redirectAttributes.addAttribute("id", course.getId());
 			
-			return "redirect:/courses";
+			return "redirect:/courses/edit";
 		}
 		
 		courseService.update(course);
 		
 		return "redirect:/courses";
+	}
+	
+	@GetMapping("/edit")
+    public String edit(
+			@RequestParam(name = "errorMessage", required = false) List<String> errorMessageList,
+    		@RequestParam(name = "id") Long id,
+    		Model model
+    		) {		
+		
+		model.addAttribute("course", courseService.findById(id));
+		model.addAttribute("errorMessageList", errorMessageList);
+		
+		return "courses/new-course";
 	}
 }
