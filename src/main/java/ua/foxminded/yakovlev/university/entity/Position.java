ec0360384938a63f4d4c8c317556a1b187148e33
Alexander Yakovlev <sashayassa@gmail.com>
2020-12-21 10:29:29 +0300

    add validation of Position

--- src/main/java/ua/foxminded/yakovlev/university/entity/Position.java
+++ src/main/java/ua/foxminded/yakovlev/university/entity/Position.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 import lombok.Data;
 
@@ -20,5 +21,6 @@ public class Position {
 	private Long id;
 	
 	@Column(name = "position_name")
+	@NotBlank(message="validator.message.empty_position_name")
 	private String name;
 }
