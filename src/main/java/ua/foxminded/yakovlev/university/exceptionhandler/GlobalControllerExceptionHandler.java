06e2f578e871a5e3c99dee5a02162f1b5e466dda
Alexander Yakovlev <sashayassa@gmail.com>
2020-12-30 19:40:14 +0300

    initial GlobalControllerExceptionHandler

---
+++ src/main/java/ua/foxminded/yakovlev/university/exceptionhandler/GlobalControllerExceptionHandler.java
@@ -0,0 +1,53 @@
+package ua.foxminded.yakovlev.university.exceptionhandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.http.HttpStatus;
+
+import ua.foxminded.yakovlev.university.exception.NotFoundException;
+import java.lang.IllegalArgumentException;
+
+@ControllerAdvice
+class GlobalControllerExceptionHandler {
+    
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseBody
+    public ExceptionInfo handleNotFound(HttpServletRequest req, NotFoundException exception) {    	    	
+		return new ExceptionInfo(req.getRequestURI(), exception.getMessage());
+    }
+    
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseBody
+    public ExceptionInfo handleIllegalArgument(HttpServletRequest req, IllegalArgumentException exception) {    	    	
+		return new ExceptionInfo(req.getRequestURI(), exception.getMessage());
+    }
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ExceptionInfo handleMethodArgumentNotValid(HttpServletRequest req, MethodArgumentNotValidException exception) {
+                          
+            BindingResult result = exception.getBindingResult();                
+            String errorsMessage = result.getAllErrors().stream()
+            		.map((e) -> e.getDefaultMessage())
+            		.reduce((s1, s2) -> s1 + "; " + s2).orElse("");
+             
+            return new ExceptionInfo(req.getRequestURI(), errorsMessage);
+    }
+    
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseBody
+    public ExceptionInfo MethodArgumentTypeMismatchException(HttpServletRequest req, MethodArgumentTypeMismatchException exception) {
+                          
+            String errorsMessage = exception.getRootCause().getMessage();
+             
+            return new ExceptionInfo(req.getRequestURI(), errorsMessage);
+    }
+}
