fb20837e154ba0b0fd2aa06c1f479e72f822317a
Alexander Yakovlev <sashayassa@gmail.com>
2021-01-27 20:53:08 +0300

    update SecurityConfig

--- src/main/java/ua/foxminded/yakovlev/university/init/SecurityConfig.java
+++ src/main/java/ua/foxminded/yakovlev/university/init/SecurityConfig.java
@@ -1,36 +1,49 @@
 package ua.foxminded.yakovlev.university.init;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.AllArgsConstructor;
+import ua.foxminded.yakovlev.university.service.impl.UserService;
 
 @EnableWebSecurity
+@AllArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private UserService userServise;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		http.authorizeRequests()
-		.antMatchers("/courses/**").authenticated()
-		.antMatchers("/timetable/**").hasRole("ADMIN")
-		.antMatchers("/students/**").hasAnyRole("STUDENT", "LECTURER")
+		http
+		.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/").permitAll()
 		.and()
 		.formLogin()
 		.and()
 		.logout().logoutSuccessUrl("/index");
 	}
 	
-	/*@Bean
-	public UserDetailsService users() {
+	@Bean
+	public PasswordEncoder passwordEncoder() {		
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
 		
-		UserDetails user = User.builder()
-				.username("Admin")
-				.password("{bcrypt}$2y$12$Cndo7HfXurCkHqytTOluPOtaPufa0Oz8c53IYmt0nu/rJelpQ5nLq")
-				.roles("ADMIN")
-				.build();
-	}*/
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setPasswordEncoder(passwordEncoder());
+		authenticationProvider.setUserDetailsService(userServise);
+		
+		return authenticationProvider;
+	}
 }
