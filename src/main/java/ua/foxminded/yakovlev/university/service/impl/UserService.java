b5303033493965b5053db7204984431a326e9591
Alexander Yakovlev <sashayassa@gmail.com>
2021-02-05 19:56:02 +0300

    update UserService

--- src/main/java/ua/foxminded/yakovlev/university/service/impl/UserService.java
+++ src/main/java/ua/foxminded/yakovlev/university/service/impl/UserService.java
@@ -2,6 +2,7 @@ package ua.foxminded.yakovlev.university.service.impl;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -14,6 +15,7 @@ import lombok.AllArgsConstructor;
 import ua.foxminded.yakovlev.university.entity.Authority;
 import ua.foxminded.yakovlev.university.entity.Role;
 import ua.foxminded.yakovlev.university.entity.User;
+import ua.foxminded.yakovlev.university.exception.NotFoundException;
 import ua.foxminded.yakovlev.university.repository.UserRepository;
 
 @Service
@@ -30,6 +32,20 @@ public class UserService implements UserDetailsService {
 		return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(), getAuthorities(user.getRoleSet()));
 	}
 	
+	public List<User> findAll() {
+		
+		return userRepository.findAll();
+	}
+	
+	public User save(User user) {
+		
+		if (userRepository.findAll().stream().anyMatch(u -> u.getName().equals(user.getName()))) {
+			throw new IllegalArgumentException("validator.message.exited_user");
+		}
+		
+		return userRepository.save(user);
+	}
+	
 	public User findByUsername(String username) {
 		
 		User user = userRepository.findByName(username);
@@ -39,6 +55,10 @@ public class UserService implements UserDetailsService {
 		}
 		
 		return user;
+	}
+	
+	public User findById(Long id) {	
+		return userRepository.findById(id).orElseThrow(() -> new NotFoundException("User with id " + id + " is not found"));
 	}
 	
 	private Collection<? extends GrantedAuthority> getAuthorities(Collection<Role> roles) {
@@ -52,5 +72,18 @@ public class UserService implements UserDetailsService {
 		}
 		
 		return authorities;
+	}
+	
+	public User update(User user) {
+
+		if (!userRepository.existsById(user.getId())) {
+			throw new NotFoundException("User with id " + user.getId() + " is not found!");
+		}
+		
+		return userRepository.saveAndFlush(user);
+	}
+	
+	public void delete(Long id) {
+		userRepository.delete(findById(id));
 	}
 }
