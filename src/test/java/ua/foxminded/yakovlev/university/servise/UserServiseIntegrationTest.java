6c3641bb4a830581e85f3bd8a1e37010f1bdb7af
Alexander Yakovlev <sashayassa@gmail.com>
2021-02-05 19:50:28 +0300

    initial UserServiseIntegrationTest

---
+++ src/test/java/ua/foxminded/yakovlev/university/servise/UserServiseIntegrationTest.java
@@ -0,0 +1,42 @@
+package ua.foxminded.yakovlev.university.servise;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ua.foxminded.yakovlev.university.exception.NotFoundException;
+import ua.foxminded.yakovlev.university.repository.StudentRepository;
+import ua.foxminded.yakovlev.university.repository.UserRepository;
+import ua.foxminded.yakovlev.university.service.impl.CourseServiceImpl;
+import ua.foxminded.yakovlev.university.service.impl.UserService;
+import ua.foxminded.yakovlev.university.util.CourseGenerator;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiseIntegrationTest {
+	
+	UserService service;
+	
+	@Mock
+	UserRepository repository;
+	
+    
+	@BeforeEach
+    public void init() {
+    	service = new UserService(repository);
+    }
+	
+	@Test
+	void findByIdShouldThrowNotFoundExceptionWhenSuchUserNotFound() {
+		when(repository.findById(anyLong())).thenReturn(Optional.empty());
+		assertThrows(NotFoundException.class, () -> service.findById(99L));
+	}
+}
