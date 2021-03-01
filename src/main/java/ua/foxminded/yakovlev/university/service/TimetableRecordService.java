c023f684d96514553a93750cadc8bb9b0d51b3aa
Alexander Yakovlev <sashayassa@gmail.com>
2021-03-01 16:05:34 +0300

    update TimetableRecordService: add method findByUser

--- src/main/java/ua/foxminded/yakovlev/university/service/TimetableRecordService.java
+++ src/main/java/ua/foxminded/yakovlev/university/service/TimetableRecordService.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import ua.foxminded.yakovlev.university.entity.TimetableRecord;
+import ua.foxminded.yakovlev.university.entity.User;
 
 public interface TimetableRecordService extends EntityService<TimetableRecord, Long>{
 
@@ -11,4 +12,6 @@ public interface TimetableRecordService extends EntityService<TimetableRecord, L
 	
 	List<TimetableRecord> findByLecturer(Long lecturerId, LocalDateTime periodStart, LocalDateTime periodFinish);
 	List<TimetableRecord> findByGroup(Long groupId, LocalDateTime periodStart, LocalDateTime periodFinish);
+	
+	List<TimetableRecord> findByUser(User user, LocalDateTime startDate, Long amountOfDays);
 }
--- src/main/java/ua/foxminded/yakovlev/university/service/impl/TimetableRecordServiceImpl.java
+++ src/main/java/ua/foxminded/yakovlev/university/service/impl/TimetableRecordServiceImpl.java
@@ -1,12 +1,16 @@
 package ua.foxminded.yakovlev.university.service.impl;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ua.foxminded.yakovlev.university.entity.TimetableRecord;
+import ua.foxminded.yakovlev.university.entity.User;
 import ua.foxminded.yakovlev.university.repository.TimetableRecordRepository;
+import ua.foxminded.yakovlev.university.service.LecturerService;
+import ua.foxminded.yakovlev.university.service.StudentService;
 import ua.foxminded.yakovlev.university.service.TimetableRecordService;
 
 @Service
@@ -16,10 +20,14 @@ public class TimetableRecordServiceImpl extends AbstractServiceJpa<TimetableReco
 	private static final String ENTITY_NAME = "Timetable record";
 	
 	private final TimetableRecordRepository dao;
+	private final LecturerService lecturerService;
+	private final StudentService studentService;
 	
-	public TimetableRecordServiceImpl(TimetableRecordRepository timetableRecordRepository)  {
+	public TimetableRecordServiceImpl(TimetableRecordRepository timetableRecordRepository, LecturerService lecturerService, StudentService studentService)  {
 		super(timetableRecordRepository);
 		this.dao = timetableRecordRepository;
+		this.lecturerService = lecturerService;
+		this.studentService = studentService;
 	}
 
 	@Override
@@ -33,8 +41,8 @@ public class TimetableRecordServiceImpl extends AbstractServiceJpa<TimetableReco
 	}
 
 	@Override
-	public List<TimetableRecord> findByGroup(Long studentId, LocalDateTime periodStart, LocalDateTime periodFinish) {
-		return dao.findByGroup(studentId, periodStart, periodFinish);
+	public List<TimetableRecord> findByGroup(Long groupId, LocalDateTime periodStart, LocalDateTime periodFinish) {
+		return dao.findByGroup(groupId, periodStart, periodFinish);
 	}
 
 	@Override
@@ -46,5 +54,39 @@ public class TimetableRecordServiceImpl extends AbstractServiceJpa<TimetableReco
 	@Override
 	protected Long getIdentifire(TimetableRecord record) {
 		return record.getId();
+	}
+
+	@Override
+	public List<TimetableRecord> findByUser(User user, LocalDateTime startDate, Long amountOfDays) {
+		
+		if (user == null) {
+			throw new IllegalArgumentException("Can't find the timetable for empty user");
+		}
+		
+		LocalDateTime now = LocalDateTime.now();
+		
+		if (startDate == null) {
+			startDate = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), 0, 0);
+		}		
+
+		if (amountOfDays == null || amountOfDays <= 0) {
+			amountOfDays = 1L;
+		}
+		
+		LocalDateTime periodStart = startDate;
+		LocalDateTime periodFinish = startDate.plusDays(amountOfDays);		
+		
+		Long personId = user.getPerson().getPersonId();
+		
+		if (lecturerService.isLecturer(personId)) {	
+			return findByLecturer(personId, periodStart, periodFinish);
+		}
+		
+		if (studentService.isStudent(personId)) {
+			Long groupId = studentService.findById(personId).getGroup().getId();
+			return findByGroup(groupId, periodStart, periodFinish);
+		}		
+
+		return Collections.emptyList();
 	}
 }
